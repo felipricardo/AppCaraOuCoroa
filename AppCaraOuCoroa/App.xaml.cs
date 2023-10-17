d03744b720f384725d51de28fb0628c170380f60
felipricardo <felipebarbosaok@gmail.com>
2023-10-17 10:13:37 -0300

    up

--- AppCaraOuCoroa/App.xaml.cs
+++ AppCaraOuCoroa/App.xaml.cs
@@ -6,6 +6,9 @@ namespace AppCaraOuCoroa
 {
     public partial class App : Application
     {
+        private DateTime lastInteractionTime;
+        private bool isRedirected;
+
         public App()
         {
             InitializeComponent();
@@ -15,14 +18,28 @@ namespace AppCaraOuCoroa
 
         protected override void OnStart()
         {
+            // Quando o aplicativo é iniciado, registre o tempo da última interação.
+            lastInteractionTime = DateTime.Now;
+            isRedirected = false;
         }
 
         protected override void OnSleep()
         {
+            // Quando o aplicativo entra em estado de suspensão (background), verifique o tempo de inatividade.
+            TimeSpan inactivityDuration = DateTime.Now - lastInteractionTime;
+            if (!isRedirected && inactivityDuration.TotalMilliseconds >= 20000) // 20 segundos
+            {
+                // O usuário está inativo por mais de 20 segundos, redirecione para a tela de login.
+                MainPage.Navigation.PushAsync(new MainPage()); // Substitua LogonPage pela sua página de login.
+                isRedirected = true;
+            }
         }
 
         protected override void OnResume()
         {
+            // Quando o aplicativo retorna ao primeiro plano, atualize o tempo da última interação.
+            lastInteractionTime = DateTime.Now;
+            isRedirected = false;
         }
     }
 }
